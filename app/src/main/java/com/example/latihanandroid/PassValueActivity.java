d81aa339a770389a0baf9fb0dcc5fc6c2fe78531
HerwinMardhana <herwin.mardhana@gmail.com>
2019-06-20 19:39:16 +0800

    Update Kedua (Belajar Intent) a.Implisit Intent (menuju ke activity diluar aplikasi sendiri) b.explisit Intent (menuju ke activity di dalam Aplikasi sendiri) (Parcelable) Plugin
    1.Membuat Activity baru (BelajarIntent,PassValueData)
    2.Pada Activity BelajarIntent ada input data kemudian di kirim ke Activity PassValueData
    3.Menampilkan Data Dari BelajarIntent
    4.Set Flag Berfungsi close aplikasi pada intent
    5.Install Plugin Parcelable

--- app/src/main/java/com/example/latihanandroid/PassValueActivity.java
+++ app/src/main/java/com/example/latihanandroid/PassValueActivity.java
@@ -8,12 +8,21 @@ import android.widget.TextView;
 public class PassValueActivity extends AppCompatActivity {
 
     TextView tvData;
+    TextView tvNim;
+    TextView tvNama;
+    TextView tvAlamat;
+    TextView tvKontak;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pass_value);
 
         tvData=findViewById(R.id.tvData);
+        tvNim=findViewById(R.id.tvNim);
+        tvNama=findViewById(R.id.tvNama);
+        tvAlamat=findViewById(R.id.tvAlamat);
+        tvKontak=findViewById(R.id.tvKontak);
 
         String data=getIntent().getStringExtra("data");
 
