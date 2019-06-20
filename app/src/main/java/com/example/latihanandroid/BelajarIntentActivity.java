fe642e2ae6547d3d7d74b63407ac70fc9360a19d
HerwinMardhana <herwin.mardhana@gmail.com>
2019-06-20 19:10:55 +0800

    Update Kedua (Belajar Intent) a.Implisit Intent (menuju ke activity diluar aplikasi sendiri) b.explisit Intent (menuju ke activity di dalam Aplikasi sendiri)
    1.Membuat Activity baru (BelajarIntent,PassValueData)
    2.Pada Activity BelajarIntent ada input data kemudian di kirim ke Activity PassValueData
    3.Menampilkan Data Dari BelajarIntent

---
+++ app/src/main/java/com/example/latihanandroid/BelajarIntentActivity.java
@@ -0,0 +1,45 @@
+package com.example.latihanandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
+
+public class BelajarIntentActivity extends AppCompatActivity {
+
+    Button bMainactivity;
+    Button bSenddata;
+    EditText inputdata;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_belajar_intent);
+
+        bMainactivity=findViewById(R.id.bMainactivity2);
+        bSenddata=findViewById(R.id.bSenddata);
+        inputdata=findViewById(R.id.inputdata);
+
+        bMainactivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(BelajarIntentActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        bSenddata.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(BelajarIntentActivity.this,PassValueActivity.class);
+                intent.putExtra("data",inputdata.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/latihanandroid/PassValueActivity.java
@@ -0,0 +1,22 @@
+package com.example.latihanandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class PassValueActivity extends AppCompatActivity {
+
+    TextView tvData;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pass_value);
+
+        tvData=findViewById(R.id.tvData);
+
+        String data=getIntent().getStringExtra("data");
+
+        tvData.setText(data);
+    }
+}
